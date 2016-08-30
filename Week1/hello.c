09ac6b74d87fbfd0fb3756734fd4962a6e824da0
elnazd <elnaz.delpisheh@gmail.com>
2016-08-30 10:16:39 -0400

    Hello world

---
+++ Week1/hello.c
@@ -0,0 +1,8 @@
+/* My first program hello.c */ 
+
+#include<stdio.h>
+int main(void) // program startup 
+{
+	 printf("Hello World!\n"); // output 			 
+	 return 0; // return to operating system 
+}
