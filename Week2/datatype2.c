49f626f83abc2556b9fd11cb8d35c5f95fe6c3f8
elnazd <elnaz.delpisheh@gmail.com>
2016-09-19 09:43:28 -0400

    Create datatype2.c

---
+++ Week2/datatype2.c
@@ -0,0 +1,29 @@
+/*Define these variables in a C code.
+Assign values to these variables.
+
+Books on loan 
+Cash in a cash register
+A student number 
+Humidity
+*/
+#include<stdio.h>
+#define pi 3.14
+#define HST 0.13
+
+int main()
+{
+	//Data
+	//Books on loan 
+	int books_on_loan = 3;
+	float cash = 100.45; 
+	int student = 4445678;
+	double humidity = 0.47;
+	
+	const float pi = 3.14;
+	//Instructions
+	//....
+
+	return 0;
+}
+
+
