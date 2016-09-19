cf7d8d9f175cc6e8e3e0c113fb15c148ccdf67c5
elnazd <elnaz.delpisheh@gmail.com>
2016-09-19 09:42:53 -0400

    Create datatypes.c

---
+++ Week2/datatypes.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	//Define a character data type to represent a letter of alphabet
+	
+	char letter = 'b';//Define a variable of type char
+	//Assign a value to the variable
+	
+	//Define a variable of type integer to store a small number
+	//Step 1: Declare the data type
+	//Step 2: Declare a variable name
+	int number = 1;
+	
+	
+	//Define a variable of type float to store PI
+	//Step 1: Declare the data type
+	//Step 2: Declare a variable name
+	
+	float pi = 3.14;
+	
+	//Define a variable of type double to store 1234.34
+	//Step 1: Declare the data type
+	//Step 2: Declare a variable name
+	double number2 = 1234.34;
+
+
+	return 0;
+	
+}
