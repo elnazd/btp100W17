00f530571f01dc3fd33d67e7d02c06b5e4afb048
elnazd <elnaz.delpisheh@gmail.com>
2016-09-25 22:10:22 -0400

    Create positives.c

---
+++ Week3/positives.c
@@ -0,0 +1,16 @@
+/*Write a program to print positive numbers from 1 to 100 on the screen:
+0,2,4,…,100.
+*/
+#include<stdio.h>
+int main()
+{
+	int counter = 0;
+	while(counter <= 100)
+	{
+		printf("%d , ",counter);
+		counter = counter + 1; //counter++;
+	}
+
+	return 0;
+
+}
