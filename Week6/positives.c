f3fd23564ee5dd2b68c8c26c6c42542f389a70e7
elnazd <elnaz.delpisheh@gmail.com>
2016-10-17 09:46:31 -0400

    Create positives.c

---
+++ Week6/positives.c
@@ -0,0 +1,13 @@
+/*Write a function to print positive integers from 1 to 100. 
+Call the function from the main function!
+*/
+
+//Make sure to download positives.h file from github and place it in the same directory as this c file.
+#include "positives.h"
+
+int main()
+{
+	//Calling the function
+	displayPositives();
+	return 0;
+}
