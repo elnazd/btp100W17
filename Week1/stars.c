e86263a3a43e0fac058198be3ebbaddfe076314c
elnazd <elnaz.delpisheh@gmail.com>
2017-01-11 23:44:55 -0500

    stary night

---
+++ Week1/stars.c
@@ -0,0 +1,28 @@
+/*
+Write a C program to create the following output:
+
+It is a starry night!
+*
+**
+***
+****
+*****
+
+*/
+#include<stdio.h>
+int main() //Entry point
+{
+    printf("It is a starry night!\n");
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    
+    //Another solution:
+    // printf("It is a starry night!\n*\n**\n***\n****\n*****\n");
+
+    
+    return 0; //Exit point
+
+}
