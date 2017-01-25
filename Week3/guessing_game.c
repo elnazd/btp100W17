b1354691be89f722d0540d160ecc04b882c5efd5
elnazd <elnaz.delpisheh@gmail.com>
2017-01-25 15:03:44 -0500

    Create guessing_game.c

---
+++ Week3/guessing_game.c
@@ -0,0 +1,25 @@
+/*Guessing game
+
+Write a program to prompt the user to enter an integer value. Compare that integer value with 100. 
+If the user’s guess is less than 100, print: “Sorry… Guess a larger number! Try again later!”. 
+If the user’s guess is greater than 100, print: “Sorry… Guess a smaller number! Try again later!”. 
+If the user’s guess is 100, print: “Correct!!”. 
+*/
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("Guess:");
+	scanf("%d",&number);
+		
+	if(number < 100)
+		printf("Sorry… Guess a larger number! Try again later!");
+	else if(number > 100)
+		printf("Sorry… Guess a smaller number! Try again later!");
+	else 
+		printf("Correct!\n");
+		
+		
+	return 0;
+
+}
